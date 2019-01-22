1515c05e731048c5e4a1f61fd39c7cde9c9d414d
James Mack <wolfbro@gmail.com>
2019-01-22 01:41:48 -0800

    MongoDB

--- web-server/server.js
+++ web-server/server.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
@@ -16,9 +44,8 @@ app.get('/', (req, res) => {
     //     ]
     // });
 
-    res.render('root.hbs', {
+    res.render('home.hbs', {
         pageTitle: 'Something',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Welcome!'
     });
 });
@@ -26,7 +53,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
     });
 });
 
@@ -36,6 +62,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
